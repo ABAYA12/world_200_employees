ccc431f435996725d498c6d6c8a19db5bbad162d
ABAYA12 <123239306+ABAYA12@users.noreply.github.com>
2024-01-25 12:46:38 +0000

    Added comments to file.py

--- file.py
+++ file.py
@@ -2,8 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+# read the csv file
 file = pd.read_csv('employees.csv')
+# convert the file to a dataframe
 file_df = pd.DataFrame(file)
+# replace null values with N/A
 file_df.fillna('N/A')
 # find the average salary
 file_df['average salary'] = file_df['salary(usd)'].mean()
@@ -16,13 +19,13 @@ file_df['avg_dept_salary'] = file_df.groupby(
 # 2 department salary percent
 file_df['dept_salary_percentage'] = ((file_df.groupby('department')[
                                      'salary(usd)'].transform('sum') / file_df['salary(usd)'].sum()) * 100).round(2)
-
+# save changes to employees.csv
 file_df.to_csv('employees.csv', index=False)
+
 # convert avg_dept_salary to an array
 avg_dept_salary = np.array(file_df['avg_dept_salary'])
 
 # plot a bar graph
-
 plt.bar(file_df['department'], avg_dept_salary)
 plt.title('World Departments Average Salary Distribution')
 plt.xlabel('Department')
