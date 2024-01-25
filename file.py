7edbd9c3f88f123e2137086f2cbd03a5e81140ad
ABAYA12 <123239306+ABAYA12@users.noreply.github.com>
2024-01-25 12:41:12 +0000

    200 employees across the globe summary stats

---
+++ file.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+file = pd.read_csv('employees.csv')
+file_df = pd.DataFrame(file)
+file_df.fillna('N/A')
+# find the average salary
+file_df['average salary'] = file_df['salary(usd)'].mean()
+
+
+# 1 What is the average salary of employees in each department?
+file_df['avg_dept_salary'] = file_df.groupby(
+    'department')['salary(usd)'].transform('mean')
+
+# 2 department salary percent
+file_df['dept_salary_percentage'] = ((file_df.groupby('department')[
+                                     'salary(usd)'].transform('sum') / file_df['salary(usd)'].sum()) * 100).round(2)
+
+file_df.to_csv('employees.csv', index=False)
+# convert avg_dept_salary to an array
+avg_dept_salary = np.array(file_df['avg_dept_salary'])
+
+# plot a bar graph
+
+plt.bar(file_df['department'], avg_dept_salary)
+plt.title('World Departments Average Salary Distribution')
+plt.xlabel('Department')
+plt.ylabel('Average Salary')
+plt.show()
